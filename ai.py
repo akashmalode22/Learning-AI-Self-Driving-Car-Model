5733d5a265dd2894ab50f6b3ba30d738615c26f9
akashmalode22 <45553815+akashmalode22@users.noreply.github.com>
2019-02-07 22:18:55 -0800

    Add files via upload
    Implemented Experience Replay (Transfer data from trial back to the start of the neural network)

--- ai.py
+++ ai.py
@@ -31,3 +31,46 @@ class Network(nn.Module):
         x = F.relu(self.fc1(state))    # Activates hidden neurons with the help of fc1. fc1 connects input to hidden layers.
         q_values = self.fc2(x)    # Output neurons of nn, put into "q_values" which has all Q values. Will put into sortmax later
         return q_values
+    
+# Experience Replay
+        
+class ReplayMemory(object):
+    
+    def __init__(self, capacity):    # Capacity here is number of memory keeps (number of previous transitions - 100)
+        self.capacity = capacity
+        self.memory = []    # Transitions are stored in this memory list of capacity "capacity"
+        
+    def push(self, event):      # "event" is put into "memory" list. "event" has --> last state, new state, last action, last reward
+        self.memory.append(event)
+        if len(self.memory) > self.capacity:       # To limit memory to have "capacity" elements
+            del self.memory[0]
+            
+    # At this point, we have random "capacity" number of samples from test cases stored inside "memory" list
+    # Now we create a function to extract random samples from the "memory" list
+    
+    def sample(self, batch_size):
+        # zip(*) reshapes list
+        # list = ((1,2,3),(4,5,6)) --> zip(*list) = ((1,4),(2,5),(3,6))
+        # memory stores here as (state, action, reward), (state, action, reward), (state, action, reward)
+        # zip of this gives --> (state, state, state), (action, action, action), (reward, reward, reward)
+        # We use zip to re-organize all states together, all actions together, all rewards together
+        samples = zip(*random.sample(self.memory, batch_size))
+        # Put samples into a PyTorch variable (--->>CHECK THIS<<---)
+        return map(lambda x: Variable(torch.cat(x, 0)), samples)  # Tensors are mult-dimensional matrix store values (0 dimensions in this case????)
+    
+        
+  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
